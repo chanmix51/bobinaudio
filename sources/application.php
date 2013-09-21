85055d46e9104878f0515065eb20e33c6bd6b34b
Grégoire HUBERT <hubert.greg@gmail.com>
2013-09-21 22:14:40 +0200

    culture support

--- sources/application.php
+++ sources/application.php
@@ -6,4 +6,27 @@ use \Symfony\Component\HttpFoundation\Response;
 $app = require "bootstrap.php";
 $app->mount('/', new Controller\MainController());
 
+$app->error(function(Exception $e, $code) use ($app) {
+
+    if ($app['debug'])
+    {
+        //return;
+    }
+
+    switch($code)
+    {
+    case "404": 
+        $error = array("fr" => "Ressource introuvable.", "en" => "Page not found.");
+        $message = array("fr" => "La ressource que vous avez demandée ne peut être trouvée.", "en" => "The page you asked for can not be found.");
+        break;
+    case "500":
+    default:
+        $error = array("fr" => "Erreur serveur.", "en" => "Server error.");
+        $message = array("fr" => "Une erreur interne a eu lieu. Nous ne pouvons donner suite à votre demande.", "en" => "An internal error occured, we can not fullfil your request, sorry.");
+    }
+
+    $culture = $app['session']->get('culture', 'fr');
+
+    return new Response($app['twig']->render('error.html.twig', array('message' => $message[$culture], 'error' => $error[$culture])), $code);
+});
 return $app;
--- sources/lib/Controller/MainController.php
+++ sources/lib/Controller/MainController.php
@@ -16,12 +16,33 @@ class MainController implements ControllerProviderInterface
         $this->app = $app;
         $controller_collection = $app['controllers_factory'];
         $controller_collection->get('/', array($this, 'index'))->bind('index');
+        $controller_collection->get('/about', array($this, 'about'))->bind('about');
+        $controller_collection->get('/culture/{culture}', array($this, 'culture'))->bind('culture');
 
         return $controller_collection;
     }
 
+    public function culture($culture)
+    {
+        $referrer = $this->app['request']->headers->get('referrer');
+
+        if (array_search($culture, array('en', 'fr')) === false)
+        {
+            $culture = 'fr';
+        }
+
+        $this->app['session']->set('culture', $culture);
+
+        return $this->app->redirect($referrer);
+    }
+
     public function index()
     {
-        return $this->app['twig']->render('index.html.twig');
+        return $this->app['twig']->render(sprintf("%s/index.html.twig", $this->app['session']->get('culture', 'fr')));
+    }
+
+    public function about()
+    {
+        return $this->app['twig']->render(sprintf("%s/about.html.twig", $this->app['session']->get('culture', 'fr')));
     }
 }
